12c2b700ed387379e1ca4fcb3bf70ce682f3d837
Jeremi Levesque <jeremilevesque@hotmail.com>
2024-06-05 09:31:44 -0400

    Mrtrix tractograms generation and updated requirements for dwi-ml

--- TractOracleNet/datasets/generate_and_score_tractograms.py
+++ TractOracleNet/datasets/generate_and_score_tractograms.py
@@ -2,6 +2,7 @@ import subprocess
 import argparse
 from pathlib import Path
 from score_tractogram import score_tractograms
+from tempfile import TemporaryDirectory
 
 fibercup_handle = "fibercup"
 ismrm2015_handle = "ismrm2015"
@@ -13,6 +14,21 @@ def validate_input(dataset, tractogram_extension, scored_extension):
         raise ValueError("Invalid extension. Please choose either 'trk' or 'tck' for the tractogram extension.")
     if scored_extension not in ["trk", "tck"]:
         raise ValueError("Invalid extension. Please choose either 'trk' or 'tck'.")
+
+def scil_tractogram_number_streamlines(tractogram_filepath):
+    subprocess.run([
+        "scil_tractogram_count_streamlines.py",
+        str(tractogram_filepath)
+    ])
+
+def scil_resample_tractogram(tractogram_filepath, out_filepath, reference, nb_streamlines):
+    subprocess.run([
+        "scil_tractogram_resample.py",
+        str(tractogram_filepath),
+        str(nb_streamlines),
+        str(out_filepath),
+        "--reference", reference
+    ])
 
 
 def scil_tracking_local(algo, dataset, out_filepath, npv, step):
@@ -41,14 +57,44 @@ def scil_tracking_pft(algo, dataset, out_filepath, npv, step):
         str(out_filepath),
     ])
 
+def mrtrix_ifod2(dataset, out_filepath, nb, step):
+    subprocess.run([
+        "tckgen",
+        "{}/fodfs/{}_fodf.nii.gz".format(dataset, dataset),
+        str(out_filepath),
+        "-algorithm", "IFOD2",
+        "-select", str(nb),
+        "-step", str(step),
+        "-minlength", "20",
+        "-maxlength", "200",
+        "-seed_image", "{}/masks/{}_interface.nii.gz".format(dataset, dataset),
+        "-nthreads", "4"
+    ])
+
+def mrtrix_sd_stream(dataset, out_filepath, nb, step):
+    subprocess.run([
+        "tckgen",
+        "{}/fodfs/{}_fodf.nii.gz".format(dataset, dataset),
+        str(out_filepath),
+        "-algorithm", "SD_Stream",
+        "-select", str(nb),
+        "-step", str(step),
+        "-minlength", "20",
+        "-maxlength", "200",
+        "-seed_image", "{}/masks/{}_interface.nii.gz".format(dataset, dataset),
+        "-nthreads", "4"
+    ])
 
 def main(args):
     dataset = args.dataset
     tractogram_extension = args.extension
     scored_extension = args.scored_extension
-    npv = args.npv
+    select = args.select
     step = args.step
     out_dir_name = args.out_dir_name
+    npv = 10
+
+    reference = "{}/anat/{}_T1.nii.gz".format(dataset, dataset) if dataset != fibercup_handle else "{}/dti/{}_fa.nii.gz".format(dataset, dataset)
 
     validate_input(dataset, tractogram_extension, scored_extension)
 
@@ -65,24 +111,53 @@ def main(args):
     # Local tracking
     scil_tracking_local_algos = ["det", "prob", "eudx"]
     for algo in scil_tracking_local_algos:
-        out_filename = "{}_local_{}.{}".format(dataset, algo, tractogram_extension)
-        out_filepath = tractograms_directory / out_filename
 
-        print("Tracking with local {}".format(algo))
-        scil_tracking_local(algo, dataset, out_filepath, npv=npv, step=step)
+        with TemporaryDirectory() as temp_dir:
+            out_filename = "{}_local_{}.{}".format(dataset, algo, tractogram_extension)
+            inter_filename = "{}_local_{}_inter.{}".format(dataset, algo, tractogram_extension)
+            out_filepath = tractograms_directory / out_filename
+            inter_filepath = Path(temp_dir) / inter_filename
+
+            print("Tracking with local {}".format(algo))
+            scil_tracking_local(algo, dataset, inter_filepath, npv=npv, step=step)
+
+            scil_tractogram_number_streamlines(inter_filepath)
+ 
+            print("Resampling tractogram to {} streamlines.".format(select))
+            scil_resample_tractogram(inter_filepath, out_filepath, reference, select)
 
         tractograms_filepaths.append(out_filepath)
 
     # PFT tracking
     scil_tracking_pft_algos = ["det", "prob"]
     for algo in scil_tracking_pft_algos:
-        out_filename = "{}_pft_{}.{}".format(dataset, algo, tractogram_extension)
-        out_filepath = tractograms_directory / out_filename
+        with TemporaryDirectory() as temp_dir:
+            out_filename = "{}_pft_{}.{}".format(dataset, algo, tractogram_extension)
+            inter_filename = "{}_pft_{}_inter.{}".format(dataset, algo, tractogram_extension)
+            out_filepath = tractograms_directory / out_filename
+            inter_filepath = Path(temp_dir) / inter_filename
 
-        print("Tracking with pft {}".format(algo))
-        scil_tracking_pft(algo, dataset, out_filepath, npv=npv, step=step)
+            print("Tracking with pft {}".format(algo))
+            scil_tracking_pft(algo, dataset, inter_filepath, npv=npv, step=step)
 
-        tractograms_filepaths.append(out_filepath)
+            scil_tractogram_number_streamlines(inter_filepath)
+
+            print("Resampling tractogram to {} streamlines.".format(select))
+            scil_resample_tractogram(inter_filepath, out_filepath, reference, select)
+
+            tractograms_filepaths.append(out_filepath)
+
+    # MRtrix IFOD2
+    ifod2_out_filepath = tractograms_directory / "{}_ifod2.{}".format(dataset, tractogram_extension)
+    print("Tracking with MRtrix IFOD2")
+    mrtrix_ifod2(dataset, ifod2_out_filepath, nb=select, step=step)
+    tractograms_filepaths.append(ifod2_out_filepath)
+
+    # MRtrix SD_Stream
+    sd_stream_out_filepath = tractograms_directory / "{}_sd_stream.{}".format(dataset, tractogram_extension)
+    print("Tracking with MRtrix SD_Stream")
+    mrtrix_sd_stream(dataset, sd_stream_out_filepath, nb=select, step=step)
+    tractograms_filepaths.append(sd_stream_out_filepath)
 
     # Make sure the scoring directory exists
     scored_tractograms_directory = tractograms_directory / "scored_tractograms"
@@ -90,7 +165,6 @@ def main(args):
         scored_tractograms_directory.mkdir()
 
     # Score tractograms using tractometer, then fusing the scores of the filtered/unfiltered streamlines into a single tractogram.
-    reference = "{}/anat/{}_T1.nii.gz".format(dataset, dataset) if dataset != fibercup_handle else "{}/dti/{}_fa.nii.gz".format(dataset, dataset)
     score_tractograms(
         reference,                                                  # reference image
         tractograms_filepaths,                                      # tractograms to score
@@ -105,7 +179,7 @@ if __name__ == "__main__":
     parser.add_argument("dataset", type=str, help="Dataset to use for generating and scoring tractograms.", choices=[fibercup_handle, ismrm2015_handle])
     parser.add_argument("--extension", type=str, default="tck", help="Extension to use for the generated tractograms.", choices=["trk", "tck"])
     parser.add_argument("--scored_extension", type=str, default="trk", help="Extension to use for the scored tractograms.", choices=["trk", "tck"])
-    parser.add_argument("--npv", type=int, default=10, help="Number of seeds per voxel to use for tracking.")
+    parser.add_argument("--select", type=int, default=100000, help="Number of streamlines to select after filtering criterions are applied.")
     parser.add_argument("--step", type=float, default=0.75, help="Step size to use for tracking.")
     parser.add_argument("--out_dir_name", type=str, default="tractograms", help="Output directory name under <dataset>/<out_dir_name> for the generated tractograms.")
     main(parser.parse_args())
